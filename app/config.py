529a882c0b3791dd7c1c0994876afdefa4f5520c
Dmitrii Sheviakov <ufocultist@icloud.com>
2025-07-20 20:16:23 -0400

    test helm flask

--- app/config.py
+++ app/config.py
@@ -2,21 +2,23 @@ import os
 
 
 class Config(object):
-    APPNAME = 'app'
+    APPNAME = os.environ.get('APP_NAME', 'app')
     ROOT = os.path.abspath(APPNAME)
-    UPLOAD_PATH = '/static/uploads'
-    SERVER_PATH = ROOT + UPLOAD_PATH
+    UPLOAD_PATH = os.environ.get('UPLOAD_PATH', '/static/uploads')
+    SERVER_PATH = os.path.join(ROOT, UPLOAD_PATH)
 
-    USER = os.environ.get('DB_USERNAME', 'smotrish_user')
-    PASSWORD = os.environ.get('DB_PASSWORD', 'ia2xwbbyby')
-    HOST = os.environ.get('DB_HOST', 'mariadb')
+    USER = os.environ.get('DB_USERNAME')
+    PASSWORD = os.environ.get('DB_PASSWORD')
+    HOST = os.environ.get('DB_HOST', 'localhost')
     PORT = os.environ.get('DB_PORT', '3306')
-    DB = os.environ.get('DB_NAME', 'smotrish_db')
+    DB = os.environ.get('DB_NAME')
 
     SQLALCHEMY_DATABASE_URI = f"mariadb+mariadbconnector://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SECRET_KEY = 'adsadaydoi38dhiu7w87dys7tasye2u7y77&@#3jd8UDOUD*#EDasdsaded3ss'
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS', 'True').lower() == 'true'
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
     SQLALCHEMY_ENGINE_OPTIONS = {
-        "pool_recycle": 1800,  # Recycle connections after 30 minutes
-        "pool_pre_ping": True  # Check connection before using it
+        "pool_recycle": int(os.environ.get("SQLALCHEMY_POOL_RECYCLE", "1800")),
+        "pool_pre_ping": os.environ.get("SQLALCHEMY_POOL_PRE_PING", "true").lower() == "true"
     }
